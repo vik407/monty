0999065090f59b6434dd821fdc90732bef120fe9
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-04 12:25:39 -0500

    dsa

--- read_file.c
+++ read_file.c
@@ -15,15 +15,17 @@ char *read_file(char *filename)
 	arg = NULL;
 	buff = malloc(sizeof(char) * buffsize);
 	if (buff == NULL)
-		exit_failure(&stack, "Error: malloc failed\n");
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 		/*TODO Malloc validate null*/
 	fd = fopen(filename, "r");
 	if (fd == NULL)
 	{	free(buff);
-		free_stack(&stack);
 		if (fd)
 			fclose(fd);
-		printf("%s %s\n", "Error: Can't open file", filename);
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
 	line = 0;
@@ -35,9 +37,7 @@ char *read_file(char *filename)
 		if (opcode)
 		{	f = opcode_handler(opcode);
 			if (f)
-			{
 				f(&stack, (unsigned int) line);
-			}
 		}
 	}
 	free(buff);
