688d36cb1b9cb5f4577f9e8cd03ca5713a3f43a1
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-04 12:39:46 -0500

    dsa

--- read_file.c
+++ read_file.c
@@ -11,33 +11,34 @@ char *read_file(char *filename)
 	size_t buffsize = 1024;
 	void (*f)(stack_t **, unsigned int);
 
-	buff = NULL;
-	arg = NULL;
+	buff = NULL, arg = NULL;
 	buff = malloc(sizeof(char) * buffsize);
 	if (buff == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
+	{	fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 		/*TODO Malloc validate null*/
 	fd = fopen(filename, "r");
 	if (fd == NULL)
 	{	free(buff);
-		if (fd)
-			fclose(fd);
 		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
 	line = 0;
 	while (getline(&buff, &buffsize, fd) != -1)
-	{
-		line++;
+	{	line++;
 		opcode = strtok(buff, "\n\t\r ");
 		arg = strtok(NULL, "\n\t\r ");
 		if (opcode)
 		{	f = opcode_handler(opcode);
 			if (f)
 				f(&stack, (unsigned int) line);
+			else
+			{	fprintf(stderr, "L%d: unknown instruction %s\n", line, opcode);
+				free_stack(&stack);
+				free(buff);
+				fclose(fd);
+			}
 		}
 	}
 	free(buff);
